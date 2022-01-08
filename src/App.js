01211337f290059ba24d484e199b03e9a9cb3f42
Kostianyn <odessavshok@gmail.com>
2022-01-08 21:40:35 +0200

    c

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import s from "./App.css";
+import s from "./App.module.css";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import Filter from "./components/Filter/Filter";
